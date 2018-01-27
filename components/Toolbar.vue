e549cd28048165ab63ad2a7292892c84878d6dcc
Nina Hwang <nihwang12@gmail.com>
2018-01-27 11:42:02 -0800

    Added toolbar and noteslist vue

---
+++ components/Toolbar.vue
@@ -0,0 +1,32 @@
+<!--
+1. Create new list
+2. Favorite/unfavorite the currently selected note
+3. Delete
+-->
+
+<template>
+  <div id="toolbar">
+    <i @click="addNote" clsas="glyphicon glyphicon-plus"></i>
+    <i @click="toggleFavorite"
+      class="glyphicon glyphicon-star"
+      :class="{starred: activeNote.favorite}"></i>
+    <i @click="deleteNote" class="glyphicon glyphicon-remove"></i>
+  </div>
+</template>
+
+<script>
+import { addNote, deleteNote, toggleFavorite } from '../vuex/actions'
+
+export default {
+  vuex: {
+    getters: {
+      activeNote: state => state.activeNote
+    },
+    actions: {
+      addNote,
+      delteNote,
+      toggleFavorite
+    }
+  }
+}
+</script>
