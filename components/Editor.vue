db3ab922d436f8013d2e61949b53a169ba86ae30
Nina Hwang <nihwang12@gmail.com>
2018-01-27 11:50:08 -0800

    added code for editor.vue

---
+++ components/Editor.vue
@@ -0,0 +1,29 @@
+<!--
+  1. Grabs current activeNote from the store and displays it in a text area
+  2. It calls the editNote() action whenever the user updates the note
+ -->
+
+ <template>
+   <div id="note-editor">
+     <textarea
+      :value="activeNoteText"
+      @input="editNote"
+      class="form-control">
+     </textarea>
+   </div>
+ </template>
+
+ <script>
+ import { editNote } from '../vuex/actions'
+
+ export default {
+  vuex: {
+    getters: {
+      activeNoteText: state => state.activeNote.text
+    },
+    actions: {
+      editNote
+    }
+  }
+ }
+ </script>
